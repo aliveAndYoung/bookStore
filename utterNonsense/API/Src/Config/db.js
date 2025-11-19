3d6ac41f5937002731c13ac319953eb3e0a96f68
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2025-11-19 15:46:01 +0200

    LOL

--- utterNonsense/API/Src/Config/db.js
+++ utterNonsense/API/Src/Config/db.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
+        console.log("connecting to DB...");
         await mongoose.connect(
             process.env.MONGO_URI ||
                 "mongodb+srv://joeking01015582672_db_user:nxqXlg2IAzXRlaCL@cluster0.ytewopf.mongodb.net/mySuperduberDatabase?appName=Cluster0"
         );
-        console.log("DB connected successfully");
     } catch (error) {
         console.log("Error in DB connection", error);
-    } 
+    }
 };
- 
--- utterNonsense/API/Src/Server.js
+++ utterNonsense/API/Src/Server.js
@@ -8,9 +8,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-connectDb();
-app.use("/api/notes", notesRoutes);
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.use((_req, _res, next) => {
+    console.log(
+        `(  ${_req.ip}  )  -> ( ${_req.method}  ) -> (  ${
+            _req.originalUrl
+        } ) @ ( ${new Date().toISOString()} ) `
+    );
+    next();
 });
+app.use("/api/notes", notesRoutes);
+connectDb()
+    .then(() => {
+        console.log("Connected to the database");
+
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database", err);
+    });
